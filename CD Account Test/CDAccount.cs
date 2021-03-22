bfb48d54dd615d92a7685b4fed1fc6cf13a6782d
Jessica Beckford <jessicabeckf@usf.edu>
2021-03-22 18:03:21 -0400

    Add project files.

---
+++ CD Account Test/CDAccount.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CD_Account_Test
+{
+    class CDAccount : SavingsAccount
+    {
+        //Fields
+        private string _maturityDate;
+
+        //Constructor
+        public CDAccount()
+        {
+            _maturityDate = "";
+        }
+
+        //MaturityDate property
+        public string MaturityRate
+        {
+            get { return _maturityDate; }
+            set { _maturityDate = value; }
+        }
+
+    }
+}
